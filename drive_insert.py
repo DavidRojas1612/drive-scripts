3eb32aaf8de6a888bfbf9f21d9b9a1a7d7362318
DavidRojas1612 <david_rojas82142@elpoli.edu.co>
2017-10-05 16:31:55 -0500

    renombrado el archivo secreto

--- drive_insert.py
+++ drive_insert.py
@@ -12,7 +12,7 @@ def autorizacion():
     store = file.Storage('storage.json')
     creds = store.get()
     if creds is None or creds.invalid:
-      flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+      flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
       creds = tools.run_flow(flow, store)
     DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
 
